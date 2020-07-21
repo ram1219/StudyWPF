b3817348965fb4417bb6b437f5d8a8ab68d89bb7
ram1219 <kimgaram0320@naver.com>
2020-07-21 11:09:57 +0900

    최초커밋

--- WPFApp/WPFApp/Properties/Resources.Designer.cs
+++ WPFApp/WPFApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WPFApp.Properties
+namespace WpfApp.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace WPFApp.Properties
 			{
 				if ((resourceMan == null))
 				{
-					global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WPFApp.Properties.Resources", typeof(Resources).Assembly);
+					global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfApp.Properties.Resources", typeof(Resources).Assembly);
 					resourceMan = temp;
 				}
 				return resourceMan;
--- WPFApp/WPFApp/Properties/Settings.Designer.cs
+++ WPFApp/WPFApp/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WPFApp.Properties
+namespace WpfApp.Properties
 {
 
 
